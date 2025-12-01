0e1d313ab7393b88c8742e7b36e4bcf5bc1906fa
Destriluk <abasaariho794@gmail.com>
2025-12-01 10:57:25 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("*************************")
+print("*****this is python*****")
+print("*************************")
+print(7)
+myname = "Ykealo"
+mylocation = "Kampala"
+myhobby = "reading novels"
+yob = 2010
+print("My name is :",myname)
+print("My location is :",mylocation)
+print("My hobby is :",myhobby)
+print("My age is :",2025-yob)
